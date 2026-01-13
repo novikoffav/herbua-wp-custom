ec7cbda1167b4616905f5c4366b81564b16da964
Andriy Novikov <novikoffav@gmail.com>
2026-01-13 19:57:32 +0200

    Update collector-country-svg-map.php

--- plugins/collector-country-svg-map/collector-country-svg-map.php
+++ plugins/collector-country-svg-map/collector-country-svg-map.php
@@ -82,7 +82,7 @@ add_shortcode('collector_country_svg_map', function($atts){
      */
     $inline_svg = <<<'SVG'
 <!-- ⬇⬇⬇ REPLACE THIS WITH YOUR REAL world.svg CONTENT ⬇⬇⬇ -->
-<svg viewBox="0 0 1000 520" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="World map placeholder">
+<svg viewBox="0 0 1000 520" xmlns:mapsvg="http://mapsvg.com" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="World map placeholder">
  
    <path
      d="m 479.68275,331.6274 -0.077,0.025 -0.258,0.155 -0.147,0.054 -0.134,0.027 -0.105,-0.011 -0.058,-0.091 0.006,-0.139 -0.024,-0.124 -0.02,-0.067 0.038,-0.181 0.086,-0.097 0.119,-0.08 0.188,0.029 0.398,0.116 0.083,0.109 10e-4,0.072 -0.073,0.119 z"
